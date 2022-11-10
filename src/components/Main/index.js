99647d1b6bf5b916230f7deceee4c06818337700
Abdiqani Mohamed <Mohamedabdiqani321@gmail.com>
2022-11-10 16:44:18 +0000

    doing task 5

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,10 +6,39 @@ import samplePosts from '../../libs/data';
 
 import Post from '../Post';
 
+// In the `<Main/>` component (src/components/Main), use the map() method to loop over the the 'posts' state. For each post in the array, return a `<Post/>` component.
+
+// const postTemplate = {
+//   postId: 0,
+//   title: 'a title',
+//   date: 'DD/MM/YY',
+//   author: 'author',
+//   text: lorem.generateParagraphs(7),
+//   highlights: [
+//     lorem.generateWords(3),
+//     lorem.generateWords(3),
+//     lorem.generateWords(3),
+//   ],
+//   image: { link: 'link', alt: 'alt' },
+
+// Set the `key` prop for each `<Post/>` component as the `postId` property contained within the post object.
+
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
-
-  return <main id="main"></main>;
+const postElement = posts.map(post=>{
+  return ( <Post 
+  key = {post.postId}
+  postID = {post.postId} 
+  title = {post.title}
+  date = {post.date}
+  author = {post.author}
+  text = {post.text}
+  highlights = {post.highlights}
+  image = {post.image}
+  />)
+})
+console.log(postElement)
+return <main id="main">{postElement}</main>;
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post(props) {
+  return <article>
+  <ul key = {props.key}>
+    <li>
+       {props.postId}
+    </li>
+    <li>
+        {props.title}
+    </li>
+    <li>
+        {props.date}
+    </li>
+    <li>
+        {props.author}
+    </li>
+    <li>
+        {props.text}
+    </li>
+    <li>
+        {props.highlights}
+    </li>
+    <li>
+        {props.image}
+    </li>
+  </ul>
+  </article>;
 }
 
 export default Post;
