6e1c172ab1f3e20b7f5dd23bc5913817a5d57619
JeremyXZ <jeremyuk168@gmail.com>
2022-11-10 20:14:41 +0000

    c ompleted but with some bugs

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,38 +6,25 @@ import samplePosts from '../../libs/data';
 
 import Post from '../Post';
 
-// In the `<Main/>` component (src/components/Main), use the map() method to loop over the the 'posts' state. For each post in the array, return a `<Post/>` component.
-
-// const postTemplate = {
-//   postId: 0,
-//   title: 'a title',
-//   date: 'DD/MM/YY',
-//   author: 'author',
-//   text: lorem.generateParagraphs(7),
-//   highlights: [
-//     lorem.generateWords(3),
-//     lorem.generateWords(3),
-//     lorem.generateWords(3),
-//   ],
-//   image: { link: 'link', alt: 'alt' },
-
-// Set the `key` prop for each `<Post/>` component as the `postId` property contained within the post object.
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
-const postElement = posts.map(post=>{
+
+  const postElement = posts.map(post=>{
+
   return ( <Post 
-  key = {post.postId}
-  postID = {post.postId} 
-  title = {post.title}
-  date = {post.date}
-  author = {post.author}
-  text = {post.text}
-  highlights = {post.highlights}
-  image = {post.image}
-  />)
+      
+      key = {post.postId}
+      postID = {post.postId} 
+      title = {post.title}
+      date = {post.date}
+      author = {post.author}
+      text = {post.text}
+      highlights = {post.highlights}
+      image = {post.image}
+      />)
 })
-console.log(postElement)
+
 return <main id="main">{postElement}</main>;
 }
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,30 +3,28 @@ import React from 'react';
 import './index.css';
 
 function Post(props) {
-  return <article>
-  <ul key = {props.key}>
-    <li>
-       {props.postId}
-    </li>
-    <li>
-        {props.title}
-    </li>
-    <li>
-        {props.date}
-    </li>
-    <li>
-        {props.author}
-    </li>
-    <li>
-        {props.text}
-    </li>
-    <li>
-        {props.highlights}
-    </li>
-    <li>
-        {props.image}
-    </li>
-  </ul>
+    
+    return <article key={props.postId} id="post">
+    
+    <h1>{props.title}</h1>
+    <h2>
+        <span>{props.author}</span>
+        <span>{props.date}</span>
+    </h2>
+    <p>{props.text}</p>
+    {props.highlights.map((item, index) => {
+            return (
+            <ul>
+                <li key={index} >
+                    {item}
+                </li>
+            </ul>  
+            )
+        })}
+    
+    <img src={props.image.link} alt={props.image.alt}/>
+    
+ 
   </article>;
 }
 
